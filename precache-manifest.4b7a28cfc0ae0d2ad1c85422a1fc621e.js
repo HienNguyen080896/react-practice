f6f10c74c41d0e5ade68e06a64b412bcb862f795
Hien Nguyen <nguyenhien8895@gmail.com>
2019-04-17 10:29:49 +0700

    Updates

---
+++ precache-manifest.4b7a28cfc0ae0d2ad1c85422a1fc621e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80917382538bb08b9aec",
+    "url": "/react-practice/static/js/runtime~main.21a1c6d2.js"
+  },
+  {
+    "revision": "ad1a13226037cac9f7e4",
+    "url": "/react-practice/static/js/main.5a1bdb2f.chunk.js"
+  },
+  {
+    "revision": "425e63bde029d967acf7",
+    "url": "/react-practice/static/js/2.9c1894cd.chunk.js"
+  },
+  {
+    "revision": "ad1a13226037cac9f7e4",
+    "url": "/react-practice/static/css/main.11d6db28.chunk.css"
+  },
+  {
+    "revision": "425e63bde029d967acf7",
+    "url": "/react-practice/static/css/2.201638f5.chunk.css"
+  },
+  {
+    "revision": "61e43b9b241fa92d69ce02e44bb8dd2c",
+    "url": "/react-practice/index.html"
+  }
+];
